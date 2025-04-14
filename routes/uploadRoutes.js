60e6030585052f343ddbd636592118d73460be7c
Diego Ovalle <you@example.com>
2025-04-14 02:01:31 -0400

    modificacion multer

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../middlewares/multer');
+const multer = require('multer');
 const authenticateToken = require('../middlewares/auth');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');},
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }});
+
+const upload = multer({ 
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5 
+  }});
 
 router.post('/upload', authenticateToken, upload.single('file'), (req, res) => {
   try {
