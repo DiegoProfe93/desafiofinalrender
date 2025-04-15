b9c837644128be43854f1fabab48794224773cea
Diego Ovalle <you@example.com>
2025-04-15 00:49:51 -0400

    modificacion new pool

--- config/db.js
+++ config/db.js
@@ -5,9 +5,13 @@ let pool;
 
 try {
   pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    ssl: process.env.DB_SSL === 'true' && {
+      rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED !== 'false'
     }
   });
   
